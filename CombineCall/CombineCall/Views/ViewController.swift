fc298682916f9d1745caf54539754cf528c26d54
Bharat Lal <bharatlal087@gmail.com>
2023-03-26 17:18:18 +0530

    added example UI update

--- CombineCall/CombineCall/Views/ViewController.swift
+++ CombineCall/CombineCall/Views/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak private var tableView: UITableView!
+    
+    private let viewModel = RepositoryViewModel()
+    private var cancellables = Set<AnyCancellable>()
+    private var repositories = [Repository]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
+        bind()
+        getData()
     }
+    
+    private func setup() {
+        tableView.dataSource = self
+    }
+    
+    private func getData() {
+        viewModel.getRepositories()
+    }
+    
+    private func bind() {
+        viewModel.$repositories.sink { _ in } receiveValue: { [weak self] repositories in
+            self?.repositories = repositories
+            self?.tableView.reloadData()
+        }
+        .store(in: &cancellables)
+    }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        repositories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RepoCell", for: indexPath) as! RepoCell
+        cell.configure(with: repositories[indexPath.item])
+        return cell
+    }
 }
 
